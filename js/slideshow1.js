707ed053b5a151cf289172bdff9db347a462d054
TubTub62 <TubTub62@users.noreply.github.com>
2018-12-14 13:31:26 +0100

    2

--- js/slideshow1.js
+++ js/slideshow1.js
@@ -3,7 +3,7 @@ carousel();
 
 function carousel() {
     var i;
-    var x = document.getElementsByClassName("slideshow");
+    var x = document.getElementsByClassName("slideshow1");
     for (i = 0; i < x.length; i++) {
        x[i].style.display = "none";  
     }

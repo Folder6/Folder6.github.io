f84a3b153953db7e44fe53ee79d3aab6990c9b78
TubTub62 <TubTub62@users.noreply.github.com>
2018-12-15 01:08:22 +0100

    script test

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,14 +1,36 @@
-var myIndex = 0;
-carousel();
+var slideIndex = 0;
+var maximum = 2;
+var minimum = 0;
+var i;
+var slides = document.getElementsByClassName("slidetext");
+var slides1 = document.getElementsByClassName("slideshow");; 
 
-function carousel() {
-    var i;
-    var x = document.getElementsByClassName("slideshow");
-    for (i = 0; i < x.length; i++) {
-       x[i].style.display = "none";  
-    }
-    myIndex++;
-    if (myIndex > x.length) {myIndex = 1}    
-    x[myIndex-1].style.display = "block";  
-    setTimeout(carousel, 2000);
+$(".mySlides").css('display','none');
+$(".mySlides1").css('display','none');
+
+
+var randomnumber1;
+var randomnumber2;
+
+showSlides();
+
+function showSlides() {
+
+    randomnumber1 = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+
+   // run this loop until randomnumber1 is different than randomnumber2
+    do {
+        randomnumber2 = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+    } while(randomnumber1 === randomnumber2);
+
+   console.log(randomnumber1 + "," + randomnumber2);
+
+    $(".mySlides").css('display','none');
+    $(".mySlides1").css('display','none');
+
+    slides[randomnumber1].style.display = "block";
+
+    slides1[randomnumber2].style.display = "block";
+
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
